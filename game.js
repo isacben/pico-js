7fc9909d1a1d24cb1bf637e2c01ea92d3b26d664
Isaac Benitez <ibsandoval@gmail.com>
2025-03-29 10:37:16 -0700

    add missing symbols

--- game.js
+++ game.js
@@ -11,23 +11,22 @@ function _update() {
 function _draw() {
     cls();
     custom_function(posX);
-    print("Hello world", 50, 50, 13); 
-    print("Isaac Ben, Sandoval", 50, 70, 13); 
+    print("<hello\\world>", 40, 60, 13); 
 }
 
 function custom_function(x) {
-   rect(0, 0, 8, 8, 2);
-   rectfill(8, 0, 8, 8, 6);
-   rect(4, 8, 8, 8, 9);
-   rect(120, 0, 8, 8, 14);
-   circ(40, x, 3, 9);
-   circfill(x, 28, 8, 13);
-   line(x, 28, 40, x, 11);
-   rectfill(69, 50, 1, 1, 9);
-   rectfill(70, 50, 1, 1, 9);
-   rectfill(71, 50, 1, 1, 9);
-   rectfill(72, 50, 1, 1, 9);
-   rectfill(73, 50, 1, 1, 9);
+   //rect(0, 0, 8, 8, 2);
+   //rectfill(8, 0, 8, 8, 6);
+   //rect(4, 8, 8, 8, 9);
+   //rect(120, 0, 8, 8, 14);
+   //circ(40, x, 3, 9);
+   //circfill(x, 28, 8, 13);
+   //line(x, 28, 40, x, 11);
+   //rectfill(69, 50, 1, 1, 9);
+   //rectfill(70, 50, 1, 1, 9);
+   //rectfill(71, 50, 1, 1, 9);
+   //rectfill(72, 50, 1, 1, 9);
+   //rectfill(73, 50, 1, 1, 9);
 }
 
 let age = 20;
